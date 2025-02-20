6a0cdeb06cdcea99fc89b9f0ff1d454ba0f33322
Pranoy Majumdar <189314177+pranoymajumdar@users.noreply.github.com>
2025-02-20 22:49:34 +0530

    lodaaaa2

--- index.ts
+++ index.ts
@@ -44,4 +44,4 @@ app.post('/upload/categories', upload.single('image'), async (req: Request, res:
     fileUrl: fileUrl
   });
 });
-app.listen(process.env.PORT!, () => console.log(`Server started in http://localhost:${process.env.PORT!}/`));
+app.listen(3000, '0.0.0.0', () => console.log(`Server started in http://localhost:3000/`));
