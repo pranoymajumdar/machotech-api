4092b3e55190085e87bd5856e2353ade0a9a9578
Pranoy Majumdar <189314177+pranoymajumdar@users.noreply.github.com>
2025-02-20 22:47:23 +0530

    lodaaaa

--- routes/products.route.ts
+++ routes/products.route.ts
@@ -36,7 +36,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits: { 
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 50 * 1024 * 1024,
     files: 10
   },
   fileFilter: (_, file, cb) => {
